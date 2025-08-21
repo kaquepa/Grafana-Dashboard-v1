972d38021b2eee5049e8fda6f036144555216aea
Domingos Graciano <domingoskakepa@hotmail.com>
2025-08-21 12:46:42 +0100

    Remove .env and .history/ files from repository tracking

--- .history/infrastructure/init_20250820155719.sql
+++
@@ -1,4 +0,0 @@
--- Garantir que o usuário existe
-CREATE USER "Postgre_Admin" WITH PASSWORD 'secure_password_123';
-CREATE DATABASE "Grafana_DB" OWNER "Postgre_Admin";
-GRANT ALL PRIVILEGES ON DATABASE "Grafana_DB" TO "Postgre_Admin";
--- .history/infrastructure/init_20250820155725.sql
+++
@@ -1,4 +0,0 @@
--- Garantir que o usuário existe
-CREATE USER "Postgres_Admin" WITH PASSWORD 'secure_password_123';
-CREATE DATABASE "Grafana_DB" OWNER "Postgres_Admin";
-GRANT ALL PRIVILEGES ON DATABASE "Grafana_DB" TO "Postgres_Admin";
